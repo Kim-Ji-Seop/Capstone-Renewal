84eafb2c21d3a31996d4578819e33d42d63c8727
Kim-Ji-Seop <jskim2x@naver.com>
2023-07-13 02:33:16 +0900

    fix: 잘못된 import수정

--- src/main/java/univ/ulsan/capstone/CapstoneApplication.java
+++ src/main/java/univ/ulsan/capstone/CapstoneApplication.java
@@ -1,6 +1,5 @@
 package univ.ulsan.capstone;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
