17bf8bef96d9f1fbde9595ba51bfc0bd9b980304
Kim-Ji-Seop <jskim2x@naver.com>
2023-07-13 04:24:18 +0900

    fix: 배포테스트

--- src/main/java/univ/ulsan/capstone/CapstoneApplication.java
+++ src/main/java/univ/ulsan/capstone/CapstoneApplication.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 
 @SpringBootApplication
 public class CapstoneApplication {
-    // 배포테스트 주석4
+    // 배포테스트 주석5
     public static void main(String[] args) {
         SpringApplication.run(CapstoneApplication.class, args);
     }
