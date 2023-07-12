22c79fe781cb693d607a96b160ebb33f346a4a70
Kim-Ji-Seop <jskim2x@naver.com>
2023-07-12 20:14:38 +0900

    fix: env주석

--- src/main/java/univ/ulsan/capstone/CapstoneApplication.java
+++ src/main/java/univ/ulsan/capstone/CapstoneApplication.java
@@ -8,20 +8,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CapstoneApplication {
 
     public static void main(String[] args) {
-        // .env 파일 로드
-        Dotenv dotenv = Dotenv.configure().load();
-
-        // 비밀 변수 사용
-        String prodDbUrl = dotenv.get("PROD_DB");
-        String rdsUsername = dotenv.get("RDS_USER_NAME");
-        String rdsPassword = dotenv.get("RDS_PW");
-        String jwtToken = dotenv.get("JWT_TOKEN");
-
-        // application.yml 파일에 적용
-        System.setProperty("spring.datasource.url", prodDbUrl);
-        System.setProperty("spring.datasource.username", rdsUsername);
-        System.setProperty("spring.datasource.password", rdsPassword);
-        System.setProperty("jwt.secret", jwtToken);
+//        // .env 파일 로드
+//        Dotenv dotenv = Dotenv.configure().load();
+//
+//        // 비밀 변수 사용
+//        String prodDbUrl = dotenv.get("PROD_DB");
+//        String rdsUsername = dotenv.get("RDS_USER_NAME");
+//        String rdsPassword = dotenv.get("RDS_PW");
+//        String jwtToken = dotenv.get("JWT_TOKEN");
+//
+//        // application.yml 파일에 적용
+//        System.setProperty("spring.datasource.url", prodDbUrl);
+//        System.setProperty("spring.datasource.username", rdsUsername);
+//        System.setProperty("spring.datasource.password", rdsPassword);
+//        System.setProperty("jwt.secret", jwtToken);
 
         SpringApplication.run(CapstoneApplication.class, args);
     }
