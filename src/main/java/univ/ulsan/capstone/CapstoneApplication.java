80b8d363cab302724818243f3f8ce7942751d875
Kim-Ji-Seop <jskim2x@naver.com>
2023-07-12 17:03:53 +0900

    fix:local proj > .env생성 및 local 환경변수 추가해서 오류 없애기

--- src/main/java/univ/ulsan/capstone/CapstoneApplication.java
+++ src/main/java/univ/ulsan/capstone/CapstoneApplication.java
@@ -1,5 +1,6 @@
 package univ.ulsan.capstone;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CapstoneApplication {
 
     public static void main(String[] args) {
+        // .env 파일 로드
+        Dotenv dotenv = Dotenv.configure().load();
+
+        // 비밀 변수 사용
+        String prodDbUrl = dotenv.get("PROD_DB");
+        String rdsUsername = dotenv.get("RDS_USER_NAME");
+        String rdsPassword = dotenv.get("RDS_PW");
+        String jwtToken = dotenv.get("JWT_TOKEN");
+
+        // application.yml 파일에 적용
+        System.setProperty("spring.datasource.url", prodDbUrl);
+        System.setProperty("spring.datasource.username", rdsUsername);
+        System.setProperty("spring.datasource.password", rdsPassword);
+        System.setProperty("jwt.secret", jwtToken);
+
         SpringApplication.run(CapstoneApplication.class, args);
     }
 
