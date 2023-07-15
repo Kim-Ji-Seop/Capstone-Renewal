66c35fc19c02c2c1531515afa203fa21c88730f1
Kim-Ji-Seop <jskim2x@naver.com>
2023-07-15 21:07:29 +0900

    :sparkles: fix: GitHub Secrets 수정

--- src/main/java/com/umc/jaetteoli/JaetteoliApplication.java
+++ src/main/java/com/umc/jaetteoli/JaetteoliApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JaetteoliApplication {
-    // 배포테스트 주석6
     public static void main(String[] args) {
         SpringApplication.run(JaetteoliApplication.class, args);
     }
