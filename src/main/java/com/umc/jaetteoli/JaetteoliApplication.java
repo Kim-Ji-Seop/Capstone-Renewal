99e6986828d8d4acc4c07363311759acc6498920
Kim-Ji-Seop <jskim2x@naver.com>
2023-07-16 00:35:26 +0900

    :sparkles: fix

--- src/main/java/com/umc/jaetteoli/JaetteoliApplication.java
+++ src/main/java/com/umc/jaetteoli/JaetteoliApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JaetteoliApplication {
-    // 배포테스트 도커-4
+    // 배포테스트 도커-5
     public static void main(String[] args) {
         SpringApplication.run(JaetteoliApplication.class, args);
     }
