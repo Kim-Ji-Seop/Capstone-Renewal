0eda5540bacaf3ac183a766cd5d85e1c55c516ca
Kim-Ji-Seop <jskim2x@naver.com>
2023-07-17 03:22:30 +0900

    :sparkles: fix: https 다시 설정

--- renew-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sleep 30 # initial delay
-
-while :; do
-    certbot renew
-    sleep 12h & wait $${!}
-    docker kill --signal=HUP nginx # signal nginx to reload its configuration
-done
