750b23d8d4df526aa684ff6621bfde5b35586fe7
Kim-Ji-Seop <jskim2x@naver.com>
2023-09-12 15:50:53 +0900

    fix: 인스턴스 ssh 포트 Anywhere 허용

--- src/main/java/com/uou/capstone/CapstoneApplication.java
+++ src/main/java/com/uou/capstone/CapstoneApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CapstoneApplication {
-    // 배포테스트 도커8
+    // 인스턴스 ssh 포트 Anywhere 허용
     public static void main(String[] args) {
         SpringApplication.run(CapstoneApplication.class, args);
     }
