d8c52e2cd68bd0ff379b5319704c2c34214ee3f9
Kim-Ji-Seop <jskim2x@naver.com>
2023-09-14 10:11:41 +0900

    feat: 웹 훅 테스트

--- src/main/java/com/uou/capstone/CapstoneApplication.java
+++ src/main/java/com/uou/capstone/CapstoneApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CapstoneApplication {
+    // 웹 훅 테스트
     public static void main(String[] args) {
         SpringApplication.run(CapstoneApplication.class, args);
     }
